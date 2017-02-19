623cd31e2c576dd9e178e70e26617b6f421f4eb5
Ryver966 <p.gorski-96@wp.pl>
2017-02-19 11:49:20 +0100

    React App

--- src/TodosList.js
+++ src/TodosList.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import '../styles/TODOList.css';
+//import '../styles/TODOList.css';
 
 export default class TodosList extends Component {
     render() {
         return (
-            <div className='add-tasks-container'>
-                <input type='text' placeholder='Tasks Field'>
-                <input type='button' value='Add Task'>
+            <div className='add-tasks'>
+                <input type='text' placeholder='Tasks Field'/>
+                <input type='button' value='Add Task'/>
             </div>
         );
-    }
-};
+    };
+}
